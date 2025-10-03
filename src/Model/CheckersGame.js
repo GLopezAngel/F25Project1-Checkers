31149b7896248fc071ef694246073f115c320ee9
Angel <lopeza06@moravian.edu>
2025-10-03 12:25:54 -0400

    added force jumps

--- src/Model/CheckersGame.js
+++ src/Model/CheckersGame.js
@@ -1,8 +1,6 @@
-/** 
- * Checkers Game logic
+/** * Checkers Game logic
  * Class will be used by to run the 2D and 3D versions of the game
- * 
- */
+ * */
 
 export class CheckersGame {
     constructor() {
@@ -10,7 +8,7 @@ export class CheckersGame {
         this.currentPlayer = 'red'; // Red starts first
         this.selectedPiece = null;
         this.validMoves = [];
-        this.mustCapture = false; // Flag to indicate if a capture is mandatory
+        this.mandatoryCaptures = this.getAllCaptureMoves(); // Find mandatory captures right away
     }
 
     /**
@@ -33,6 +31,27 @@ export class CheckersGame {
     }
 
     /**
+     * Get all possible capture moves for the current player.
+     * @returns {Array} array of pieces that can make a capture [{ row, col }]
+     */
+    getAllCaptureMoves() {
+        const capturablePieces = [];
+        for (let row = 0; row < 8; row++) {
+            for (let col = 0; col < 8; col++) {
+                const piece = this.board[row][col];
+                if (piece && piece.color === this.currentPlayer) {
+                    const moves = this.getValidMoves(row, col);
+                    // A capture move is one where the move distance is 2
+                    if (moves.some(move => Math.abs(move.row - row) === 2)) {
+                        capturablePieces.push({ row, col });
+                    }
+                }
+            }
+        }
+        return capturablePieces;
+    }
+
+    /**
      * Select a piece on the board.
      * @param {number} row the row of the piece
      * @param {number} col the column of the piece
@@ -41,9 +60,27 @@ export class CheckersGame {
     selectPiece(row, col) {
         const piece = this.board[row][col];
         if (piece && piece.color === this.currentPlayer) {
+
+            // Check if a capture is mandatory
+            if (this.mandatoryCaptures.length > 0) {
+                // If a capture is mandatory, ONLY allow selection of pieces that can capture
+                const isCapturingPiece = this.mandatoryCaptures.some(p => p.row === row && p.col === col);
+                if (!isCapturingPiece) {
+                    console.log("Must select a piece that can capture!");
+                    return false;
+                }
+            }
+            
             this.selectedPiece = { row, col };
             this.validMoves = this.getValidMoves(row, col);
-            return true;
+
+            // Filter moves: if captures exist for this piece, only allow those
+            const potentialCaptures = this.validMoves.filter(move => Math.abs(move.row - row) === 2);
+            if (potentialCaptures.length > 0) {
+                this.validMoves = potentialCaptures; // Force capture for this piece
+            }
+            
+            return this.validMoves.length > 0; // Only select if there are valid moves
         }
         return false;
     }
@@ -58,6 +95,8 @@ export class CheckersGame {
 
     /**
      * Get valid moves for a piece at a given position.
+     * NOTE: This returns ALL possible moves (normal and capture) for this piece.
+     * The logic in selectPiece/makeMove determines which are *valid* based on mandatory capture rules.
      * @param {number} row the row of the piece
      * @param {number} col the column of the piece
      * @returns {Array} array of valid move positions [{ row, col }]
@@ -71,13 +110,12 @@ export class CheckersGame {
             : (piece.color === 'red' ? [[-1, 1], [-1, -1]] : [[1, 1], [1, -1]]);
 
         const moves = [];
-        const captures = [];
 
         for (const [dRow, dCol] of directions) {
             const newRow = row + dRow;
             const newCol = col + dCol;
 
-            // Check for normal move
+            // Check for normal move (only if not a capture)
             if (this.isInBounds(newRow, newCol) && !this.board[newRow][newCol]) {
                 moves.push({ row: newRow, col: newCol });
             }
@@ -89,13 +127,10 @@ export class CheckersGame {
                 this.board[newRow][newCol] &&
                 this.board[newRow][newCol].color !== piece.color &&
                 !this.board[jumpRow][jumpCol]) {
-                captures.push({ row: jumpRow, col: jumpCol });
+                moves.push({ row: jumpRow, col: jumpCol }); // Capture move added
             }
         }
-
-        // If captures are available, they must be taken
-        return captures.length > 0 ? captures : moves;
-
+        return moves;
     }
 
     /**
@@ -116,30 +151,42 @@ export class CheckersGame {
         // Move the piece
         this.board[row][col] = piece;
         this.board[selRow][selCol] = null;
+        
+        const isCapture = Math.abs(row - selRow) === 2;
 
         // Check for captures
-        if (Math.abs(row - selRow) === 2) {
+        if (isCapture) {
             const capRow = (row + selRow) / 2;
             const capCol = (col + selCol) / 2;
             this.board[capRow][capCol] = null; // Remove captured piece
         }
 
         // Promote to king if reaching the opposite end
-        if ((piece.color === 'red' && row === 0) || (piece.color === 'black' && row === 7)) {
+        if (!piece.isKing && ((piece.color === 'red' && row === 0) || (piece.color === 'black' && row === 7))) {
             piece.isKing = true;
         }
 
-        // Check for additional captures
-        this.selectedPiece = { row, col };
-        this.validMoves = this.getValidMoves(row, col);
-        if (this.validMoves.some(move => Math.abs(move.row - row) === 2)) {
-            // Must continue capturing
-            return true;
+
+        // Check for **additional captures** for the same piece (chain jump)
+        if (isCapture) {
+            this.selectedPiece = { row, col };
+            const nextMoves = this.getValidMoves(row, col);
+            const nextCaptures = nextMoves.filter(move => Math.abs(move.row - row) === 2);
+            
+            if (nextCaptures.length > 0) {
+                // Must continue capturing with the same piece
+                this.validMoves = nextCaptures;
+                // Note: Don't switch turns yet, just return true to indicate a successful move
+                return true;
+            }
         }
 
-        // Switch turns
+
+        // End of turn: Switch player and re-evaluate mandatory captures
         this.currentPlayer = this.currentPlayer === 'red' ? 'black' : 'red';
         this.deselectPiece();
+        this.mandatoryCaptures = this.getAllCaptureMoves();
+        
         return true;
     }
 
@@ -149,19 +196,15 @@ export class CheckersGame {
 
     isInBounds(row, col) {
         return row >= 0 && row < 8 && col >= 0 && col < 8;
-
-    
     }
 
-    /** 
-     * resets the game to initial state
+    /** * resets the game to initial state
      */
     resetGame() {
         this.board = this.initializeBoard();
         this.currentPlayer = 'red';
         this.selectedPiece = null;
         this.validMoves = [];
-        this.mustCapture = false;
+        this.mandatoryCaptures = this.getAllCaptureMoves();
     }
- 
 }
